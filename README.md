62528d9cdbb0fc3f7fbc2ab4e8aa63abbdae4813
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-05-03 10:18:13 +0900

    added uart.py

--- README.md
+++ README.md
@@ -312,4 +312,35 @@ Pressure    : 1004.69 hPa
 ```
 
 
+# UART IO example   
 
+### Hardware requirements
+USB-TTL converter like CH340.
+
+### Wirering
+
+|CH340||GPIO||
+|:-:|:-:|:-:|:-:|
+|Gnd|--|Gnd||
+|TX|--|RX GPIO||
+|RX|--|TX GPIO||
+
+
+```
+$ sudo -E python3 uart.py --help
+usage: uart.py [-h] [-d DEVICE] [-b BAUDRATE] [-p PRINT]
+
+options:
+  -h, --help            show this help message and exit
+  -d DEVICE, --device DEVICE
+                        device file name
+  -b BAUDRATE, --baudrate BAUDRATE
+                        baudrate
+  -p PRINT, --print PRINT
+                        print debug
+
+$ sudo -E python3 uart.py -d /dev/ttyS3 -b 115200
+device=/dev/ttyS3
+baudrate=115200
+read 5 bytes: [hello]
+```
---
+++ uart.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+#-*- encoding: utf-8 -*-
+import signal
+import argparse
+from periphery import Serial
+
+serial = None
+
+def handler(signal, frame):
+    global running
+    print('handler')
+    running = False
+
+if __name__=="__main__":
+	signal.signal(signal.SIGINT, handler)
+	running = True
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-d', '--device', help="device file name", default='/dev/tty')
+	parser.add_argument('-b', '--baudrate', type=int, help="baudrate", default=115200)
+	parser.add_argument('-p', '--print', type=int, help="print debug", default=0)
+	args = parser.parse_args()
+
+	
+	# Open device with baudrate 115200, and defaults of 8N1, no flow control
+	print("device={}".format(args.device))
+	print("baudrate={}".format(args.baudrate))
+	#serial = Serial("/dev/ttyS4", 115200)
+	serial = Serial(args.device, args.baudrate)
+	DEBUG = args.print
+
+	while running:
+		serial.write(b"Hello World!")
+
+		# Read up to 128 bytes with 1000ms timeout
+		buf = serial.read(128, 1.0)
+		if(DEBUG): print("len(buf)={}".format(len(buf)))
+		if (len(buf) == 0):
+			continue
+		if(DEBUG): print("type(buf)={}".format(type(buf)))
+		if (type(buf) is bytes):
+			buf=buf.decode('utf-8')
+		print("read {:d} bytes: [{}]".format(len(buf), buf))
+
+	serial.close()
+
