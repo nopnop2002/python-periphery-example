0ddba4082396d83f3f203fdfd20d5e1cabf354b4
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-05-09 17:42:57 +0900

    Added list of text arguments

--- hdd44780/README.md
+++ hdd44780/README.md
@@ -3,27 +3,31 @@
 ```
 $ sudo -E python3 hdd44780.py --help
 usage: hdd44780.py [-h] [--rs RS] [--enable ENABLE] [--d4 D4] [--d5 D5] [--d6 D6] [--d7 D7] [--text1 TEXT1] [--text2 TEXT2]
-                   [--text3 TEXT3] [--text4 TEXT4] [--clear CLEAR]
+                   [--text3 TEXT3] [--text4 TEXT4] [--texts [TEXTS ...]]
 
 optional arguments:
-  -h, --help       show this help message and exit
-  --rs RS          RS GPIO
-  --enable ENABLE  ENABLE GPIO
-  --d4 D4          D4 GPIO
-  --d5 D5          D4 GPIO
-  --d6 D6          D4 GPIO
-  --d7 D7          D4 GPIO
-  --text1 TEXT1    text of line1
-  --text2 TEXT2    text of line2
-  --text3 TEXT3    text of line3
-  --text4 TEXT4    text of line4
-  --clear CLEAR    clear all text
+  -h, --help           show this help message and exit
+  --rs RS              RS GPIO
+  --enable ENABLE      ENABLE GPIO
+  --d4 D4              D4 GPIO
+  --d5 D5              D4 GPIO
+  --d6 D6              D4 GPIO
+  --d7 D7              D4 GPIO
+  --text1 TEXT1        text of line1
+  --text2 TEXT2        text of line2
+  --text3 TEXT3        text of line3
+  --text4 TEXT4        text of line4
+  --texts [TEXTS ...]  a list of text
 
 ## for 1602 LCD
 $ sudo -E python3 hdd44780.py --rs 23 --enable 24 --d4 14 --d5 15 --d6 17 --d7 18 --text1 'Hello!!' --text2 'World!!'
 
+$ sudo -E python3 hdd44780.py --rs 23 --enable 24 --d4 14 --d5 15 --d6 17 --d7 18 --texts 'Hello!!' 'World!!'
+
 ## for 1604 LCD
 $ sudo -E python3 hdd44780.py --rs 23 --enable 24 --d4 14 --d5 15 --d6 17 --d7 18 --text1 'Hello!!' --text2 'World!!' --text3 'Good!!' --text4 'Bye!!'
 
+$ sudo -E python3 hdd44780.py --rs 23 --enable 24 --d4 14 --d5 15 --d6 17 --d7 18 --texts 'Hello!!' 'World!!' 'Good!!' 'Bye!!'
+
 ## for clear all text
-$ sudo -E python3 hdd44780.py --rs 23 --enable 24 --d4 14 --d5 15 --d6 17 --d7 18 --clear
+$ sudo -E python3 hdd44780.py --rs 23 --enable 24 --d4 14 --d5 15 --d6 17 --d7 18
--- hdd44780/hdd44780.py
+++ hdd44780/hdd44780.py
@@ -13,32 +13,40 @@ if __name__ == '__main__':
 	parser.add_argument('--d5', type=int, help="D4 GPIO", default=15)
 	parser.add_argument('--d6', type=int, help="D4 GPIO", default=17)
 	parser.add_argument('--d7', type=int, help="D4 GPIO", default=18)
-	parser.add_argument('--text1', help="text of line1", default='Hello!!')
-	parser.add_argument('--text2', help="text of line2", default='World!!')
+	parser.add_argument('--text1', help="text of line1", default='')
+	parser.add_argument('--text2', help="text of line2", default='')
 	parser.add_argument('--text3', help="text of line3", default='')
 	parser.add_argument('--text4', help="text of line4", default='')
-	parser.add_argument('--clear', action='store_true', help="clear all text")
+	parser.add_argument('--texts', type=str, nargs="*", help='a list of text')
 	args = parser.parse_args()	
-	#print("clear={}".format(args.clear))
 	
+	text1 = args.text1
+	text2 = args.text2
+	text3 = args.text3
+	text4 = args.text4
+	if args.texts is not None:
+		print("texts={} {}".format(len(args.texts),args.texts))
+		text1 = args.texts[0]
+		if len(args.texts) > 1: text2 = args.texts[1]
+		if len(args.texts) > 2: text3 = args.texts[2]
+		if len(args.texts) > 3: text4 = args.texts[3]
+
 	lcd = LcdGpio(args.rs, args.enable, args.d4, args.d5, args.d6, args.d7)
 
 	# Inisialize display
 	lcd.init()
 	# Clear display
 	lcd.clear()
-	if args.clear is True:
-		sys.exit()
 	# Show display
-	len1 = len(args.text1)
-	len2 = len(args.text2)
-	len3 = len(args.text3)
-	len4 = len(args.text4)
+	len1 = len(text1)
+	len2 = len(text2)
+	len3 = len(text3)
+	len4 = len(text4)
 	if len3 or len4:
-		lcd.string(1, args.text1)
-		lcd.string(2, args.text2)
-		lcd.string(3, args.text3)
-		lcd.string(4, args.text4)
+		lcd.string(1, text1)
+		lcd.string(2, text2)
+		lcd.string(3, text3)
+		lcd.string(4, text4)
 	elif len1 or len2:
-		lcd.string(1, args.text1)
-		lcd.string(2, args.text2)
+		lcd.string(1, text1)
+		lcd.string(2, text2)
