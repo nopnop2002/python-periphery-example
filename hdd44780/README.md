f41139465a97b328816762c22ad0773aad3b5e9a
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-05-09 17:14:32 +0900

    added clear all text

--- hdd44780/README.md
+++ hdd44780/README.md
@@ -3,7 +3,7 @@
 ```
 $ sudo -E python3 hdd44780.py --help
 usage: hdd44780.py [-h] [--rs RS] [--enable ENABLE] [--d4 D4] [--d5 D5] [--d6 D6] [--d7 D7] [--text1 TEXT1] [--text2 TEXT2]
-                   [--text3 TEXT3] [--text4 TEXT4]
+                   [--text3 TEXT3] [--text4 TEXT4] [--clear CLEAR]
 
 optional arguments:
   -h, --help       show this help message and exit
@@ -17,9 +17,13 @@ optional arguments:
   --text2 TEXT2    text of line2
   --text3 TEXT3    text of line3
   --text4 TEXT4    text of line4
+  --clear CLEAR    clear all text
 
 ## for 1602 LCD
-$ sudo -E $ python3 hdd44780.py --rs 23 --enable 24 --d4 14 --d5 15 --d6 17 --d7 18 --text1 "Hello!" --text2 "World!"
+$ sudo -E python3 hdd44780.py --rs 23 --enable 24 --d4 14 --d5 15 --d6 17 --d7 18 --text1 "Hello!" --text2 "World!"
 
 ## for 1604 LCD
-$ sudo -E $ python3 hdd44780.py --rs 23 --enable 24 --d4 14 --d5 15 --d6 17 --d7 18 --text1 "Hello!" --text2 "World!" --text3 "Good!" --text4 "Bye!"
+$ sudo -E python3 hdd44780.py --rs 23 --enable 24 --d4 14 --d5 15 --d6 17 --d7 18 --text1 "Hello!" --text2 "World!" --text3 "Good!" --text4 "Bye!"
+
+## for clear all text
+$ sudo -E python3 hdd44780.py --rs 23 --enable 24 --d4 14 --d5 15 --d6 17 --d7 18 --clear
--- hdd44780/hdd44780.py
+++ hdd44780/hdd44780.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 #-*- encoding: utf-8 -*-
+import sys
 import argparse
 import time
 from LcdGpio import LcdGpio
@@ -14,9 +15,11 @@ if __name__ == '__main__':
 	parser.add_argument('--d7', type=int, help="D4 GPIO", default=18)
 	parser.add_argument('--text1', help="text of line1", default='Hello!!')
 	parser.add_argument('--text2', help="text of line2", default='World!!')
-	parser.add_argument('--text3', help="text of line3", default='Hello!!')
-	parser.add_argument('--text4', help="text of line4", default='Japan!!')
+	parser.add_argument('--text3', help="text of line3", default='')
+	parser.add_argument('--text4', help="text of line4", default='')
+	parser.add_argument('--clear', action='store_true', help="clear all text")
 	args = parser.parse_args()	
+	#print("clear={}".format(args.clear))
 	
 	lcd = LcdGpio(args.rs, args.enable, args.d4, args.d5, args.d6, args.d7)
 
@@ -24,7 +27,18 @@ if __name__ == '__main__':
 	lcd.init()
 	# Clear display
 	lcd.clear()
+	if args.clear is True:
+		sys.exit()
 	# Show display
-	if len(args.text1) or len(args.text2):
+	len1 = len(args.text1)
+	len2 = len(args.text2)
+	len3 = len(args.text3)
+	len4 = len(args.text4)
+	if len3 or len4:
+		lcd.string(1, args.text1)
+		lcd.string(2, args.text2)
+		lcd.string(3, args.text3)
+		lcd.string(4, args.text4)
+	elif len1 or len2:
 		lcd.string(1, args.text1)
 		lcd.string(2, args.text2)
